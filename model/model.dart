29f30918aac036f5b72f6603234404ee3a68385b
ardian <72329921+ardianrachman12@users.noreply.github.com>
2023-01-04 22:35:23 +0700

    Add files via upload

---
+++ model/model.dart
@@ -0,0 +1,92 @@
+class DaftarGot {
+  String name;
+  String city;
+  String date;
+  String actor;
+  String description;
+  String imageURL;
+  List<String> imageUrls;
+
+  DaftarGot({
+    required this.name,
+    required this.city,
+    required this.date,
+    required this.actor,
+    required this.description,
+    required this.imageURL,
+    required this.imageUrls,
+  });
+}
+
+var daftarGameOfThrones = [
+  DaftarGot(
+      name: "Kit Harington",
+      city: "London",
+      date: "26 Desember 1986",
+      actor: "Jon Snow",
+      description:
+          "Christopher Catesby Harington (lahir 26 Desember 1986) adalah seorang aktor Inggris yang dikenal luas karena perannya sebagai Jon Snow dalam serial televisi fantasi HBO Game of Thrones (2011–2019). \n\nSetelah belajar di Royal Central School of Speech and Drama , Harington melakukan debut akting profesionalnya dengan peran utama Albert Narracott dalam drama West End War Horse pada tahun 2009. Dia telah menerima beberapa penghargaan, termasuk nominasi Golden Globe Award dan dua Primetime nominasi Penghargaan Emmy . Harington mengembangkan, memproduksi, dan membintangi serial drama BBC Gunpowder 2017 , berdasarkan peran utama leluhurnya Robert Catesby dalam plot Gunpowder . Peran filmnya termasuk film roman sejarah Pompeii(2014), drama sejarah Inggris Testament of Youth (2014), dan film Marvel Cinematic Universe Eternals (2021). Dia juga mengisi suara Eret di film kedua dan ketiga dari serial film How to Train Your Dragon (2014–19).",
+      imageURL: "https://tinyurl.com/3fjwetfw",
+      imageUrls: [
+        "https://tinyurl.com/y6r5zkba",
+        "https://tinyurl.com/kax5sd9d",
+        "https://tinyurl.com/yts4r6tp",
+        "https://tinyurl.com/mt7zkarv"
+      ]),
+  DaftarGot(
+      name: "Emilia Clarke",
+      city: "London",
+      date: "23 Oktober 1986",
+      actor: "Daenerys Targaryen",
+      description:
+          "Emilia Isobel Euphemia Rose Clarke (lahir 23 Oktober 1986) adalah seorang aktris Inggris. Dia terkenal karena perannya sebagai Daenerys Targaryen di Game of Thrones .\n\nDia telah menerima berbagai penghargaan, termasuk Empire Award , Saturn Award , tiga nominasi Critics' Choice Award , dan empat nominasi Primetime Emmy Award . Pada 2019, Time menamainya sebagai salah satu dari 100 orang paling berpengaruh di dunia .Clarke belajar di Drama Centre London , tampil di sejumlah produksi panggung. Debut televisinya adalah penampilan tamu di opera sabun medis BBC One Doctors pada tahun 2009. Tahun berikutnya, dia dinobatkan sebagai salah satu 'Bintang Masa Depan Inggris' oleh majalah Screen International untuk perannya dalam film TV Serangan Trias (2010 ). Clarke memiliki peran terobosannya sebagai Daenerys Targaryen dalam serial televisi fantasi HBO Game of Thrones (2011–2019)",
+      imageURL: "https://s.id/1uJ0G",
+      imageUrls: [
+        "https://s.id/1uJ1p",
+        "https://tinyurl.com/y2jn9fcn",
+        "https://tinyurl.com/k5v6cp7s",
+        "https://tinyurl.com/7vdpvc4b"
+      ]),
+  DaftarGot(
+      name: "Sophie Turner",
+      city: "Chesterton",
+      date: "21 Februari 1996",
+      actor: "Sansa Stark",
+      description:
+          "Sophie Belinda Jonas (née Turner; lahir 21 Februari 1996) adalah aktris asal Inggris. Dia terkenal karena memerankan Sansa Stark di serial televisi fantasi HBO, Game of Thrones (2011–2019), yang membuatnya mendapatkan nominasi Primetime Emmy Award untuk Aktris Pendukung Luar Biasa dalam Seri Drama pada 2019.\n\nTurner muncul di film televisi The Thirteenth Tale (2013) dan membuat debut film tayang bioskop di Another Me (2013). Dia muncul dalam komedi aksi Barely Lethal (2015) dan memerankan Jean Gray di X-Men: Apocalypse (2016) dan Phoenix di X-Men: Dark Phoenix (2019).Dia telah ditampilkan dalam pekerjaan untuk merek desainer mewah Louis Vuitton.",
+      imageURL: "https://tinyurl.com/5n777wax",
+      imageUrls: [
+        "https://tinyurl.com/bd5ehxee",
+        "https://tinyurl.com/4m9hpd7b",
+        "https://tinyurl.com/3wyd4x6a",
+        "https://tinyurl.com/3nbhy49e"
+      ]),
+  DaftarGot(
+      name: "Peter Dinkage",
+      city: "New Jersey",
+      date: "11 Juni 1969",
+      actor: "Tyrion Lannister",
+      description:
+          "Peter Hayden Dinklage (lahir 11 Juni 1969) adalah aktor film, televisi, dan panggung Amerika. Dia menerima pengakuan internasional untuk memerankan Tyrion Lannister di serial televisi HBO Game of Thrones (2011–2019), di mana dia memenangkan Primetime Emmy Award untuk Aktor Pendukung Luar Biasa dalam Serial Drama sebanyak empat kali. [4] Ia juga menerima Penghargaan Golden Globe pada tahun 2011 dan Penghargaan Screen Actors Guild pada tahun 2020 untuk peran tersebut.\n\n Dinklage belajar akting di Bennington College , tampil di sejumlah produksi panggung amatir. Dia membuat debut filmnya dalam komedi hitam Living in Oblivion (1995), dan melakukan terobosan dengan peran utama dalam drama komedi tahun 2003 The Station Agent . Film lainnya termasuk Elf (2003), Lassie (2005), Find Me Guilty (2006), Penelope (2006), Underdog (2007), Death at a Funeral (2007), The Chronicles of Narnia: Prince Caspian (2008), Kematian di Pemakaman (2010), X-Men: Days of Future Past (2014),Pixels (2015), dan Three Billboards Outside Ebbing, Missouri (2017). Pada tahun 2018, ia tampil sebagai Eitri dalamfilm Marvel Avengers: Infinity War dan Hervé Villechaize dalamfilm biografi My Dinner with Hervé . Dia juga mengisi suara untuk video game Destiny .\n\n Dinklage memiliki bentuk umum dari dwarfisme yang dikenal sebagai achondroplasia ; tingginya 4 kaki 5 inci (1,35 m). Dia telah menggunakan status selebritasnya untuk meningkatkan kesadaran sosial tentang kurcaci.",
+      imageURL: "https://tinyurl.com/9tp8dxn8",
+      imageUrls: [
+        "https://tinyurl.com/mtuhr3s7",
+        "https://tinyurl.com/ys5jbstt",
+        "https://tinyurl.com/5yzs7wkt",
+        "https://tinyurl.com/ybnmr5kp"
+      ]),
+  DaftarGot(
+      name: "Lena Headey",
+      city: "Hamilton",
+      date: "3 Oktober 1973",
+      actor: "Cersei Lannister",
+      description:
+          "Lena Kathren Headey (lahir 3 Oktober 1973) adalah seorang aktris Inggris. Ia memperoleh pengakuan dan pengakuan internasional atas perannya sebagai Cersei Lannister dalam serial drama fantasi epik HBO Game of Thrones (2011–2019), di mana ia menerima lima nominasi Primetime Emmy Award dan nominasi Golden Globe Award , dan Queen Gorgo pada tahun 300 ( 2006).\n\nHeadey membuat debut filmnya di film Inggris Pakaian di Lemari (AS: Rumah Musim Panas ) (1992) bersama film hebat Jeanne Moreau , Joan Plowright , dan Julie Walters . [1] [2] Ini diikuti oleh drama misteri Waterland (1992). Dia terus bekerja dengan mantap di film Inggris dan Amerika dan di televisi, sebelum mendapatkan pengakuan lebih lanjut dengan penampilan utamanya di film The Brothers Grimm (2005) dan 300 (2006). Kredit film lainnya termasuk The Remains of the Day (1993), The Jungle Book (1994), Mrs Dalloway(1997), Ripley's Game (2002), Imagine Me & You (2005), Dredd (2012), The Purge (2013), 300: Rise of an Empire (2014), Pride and Prejudice and Zombies (2016), dan Fighting dengan Keluarga Saya (2019).\n\nDi luar film, Headey berperan sebagai Sarah Connor dalam serial televisi fiksi ilmiah Terminator: The Sarah Connor Chronicles (2008–2009) dan memiliki peran berulang sebagai Amelia Hughes dalam serial web animasi Kereta Infinity (2019–2021). Dia mengisi suara untuk permainan video role-playing Risen (2009) dan film tie-in video game Kingsglaive: Final Fantasy XV (2016), serta serial animasi Danger Mouse (2015–2017) dan Trollhunters (2017–2018 ). ), dan serial boneka The Dark Crystal: Age of Resistance (2019).",
+      imageURL: "https://tinyurl.com/mryfurmh",
+      imageUrls: [
+        "https://tinyurl.com/5y8adf3a",
+        "https://tinyurl.com/2zc38sba",
+        "https://tinyurl.com/3h32p2az",
+        "https://tinyurl.com/54tynm3a"
+      ])
+];
